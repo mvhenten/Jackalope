77b57afc6783de011a424e6a1920c83fa69db10f
Matthijs van Henten <matthijs@ischen.nl>
2013-02-27 13:03:35 +0100

    bark if test fails

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,18 @@ SCRIPT_DIR=`dirname $0`
 PROJECT_PATH=`dirname ${SCRIPT_DIR}` 
 TEST_DIR=`dirname ${SCRIPT_DIR}`/t
 
+assert_tests_ok () {
+    if [ $? -ne 0 ]
+    then
+        echo "\nBUILD FAILED\nTest returned no-zero exit status, build failed\n"
+        exit 1;
+    fi    
+}
+
 cat $PROJECT_PATH/src/*coffee | coffee -s -p > $PROJECT_PATH/lib/jackalope.js
 
 coffee $TEST_DIR/Jackalope.coffee
+assert_tests_ok
+
 coffee $TEST_DIR/TypeConstraints.coffee
+assert_tests_ok
